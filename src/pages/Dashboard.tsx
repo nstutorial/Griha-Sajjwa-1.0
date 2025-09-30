e45e4f62221cc533badcdddba092f7693aa0ff98
Nsutradhar <102289132+nstutorial@users.noreply.github.com>
2025-09-30 14:43:03 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -175,7 +175,7 @@ const Dashboard = () => {
               <Receipt className="h-4 w-4" />
               Payments
             </TabsTrigger>
-            <TabsTrigger value="security" className="flex items-center gap-2">
+            <TabsTrigger value="security" className="flex items-center gap-2 hidden">
               <Shield className="h-4 w-4" />
               Security
             </TabsTrigger>
